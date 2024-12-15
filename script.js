59460d300252d705515f9b83115a7f003cbfeb0d
Ali Jazbi <ali.jazbi@yahoo.com>
2024-12-16 01:57:16 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let openModel = function () {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden-bg");
+};
+let closeModal = function () {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden-bg");
+};
+document.addEventListener("keydown", function (e) {
+	if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
+document.querySelector(".show-modal").addEventListener("click", openModel);
+document.querySelector(".close-modal").addEventListener("click", closeModal);
